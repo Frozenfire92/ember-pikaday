11d9e1501378e2721a35fca7b4ec7da1fb37fe4b
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-11-06 15:31:47 -0400

    Add placeholder changes to source and prepare new release.

--- build/lib.js
+++ build/lib.js
@@ -1,7 +1,7 @@
 (function() {
 
 Ember.Pikaday = Ember.Namespace.create();
-Ember.Pikaday.VERSION = '0.1.5';
+Ember.Pikaday.VERSION = '0.1.7';
 
 Ember.libraries.register('Ember Pikaday', Ember.Pikaday.VERSION);
 
